c875fccab11f486af60a7c88d7f69e2619418742
marcusvlc <marcus.costa@ccc.ufcg.edu.br>
2017-11-10 16:30:12 -0200

    Fazendo duas novas paginas, para listar as musicas e albuns do sistema

--- js/app/app.js
+++ js/app/app.js
@@ -8,11 +8,11 @@ app.factory('todoListService', function() {
     , comentario: "Artista pré-cadastrado para testes", ehFavorito: false, albuns: [], nota:"0", ultimaMusica:""}
     ];
 
-    var albunsCadastrados = [];
+    var albunsCadastrados = [ {nome: "Whats the story?", imagem: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b1/Oasis_-_%28What%27s_The_Story%29_Morning_Glory_album_cover.jpg/220px-Oasis_-_%28What%27s_The_Story%29_Morning_Glory_album_cover.jpg", ano: "1995", musicas:[], dono:"Oasis"}];
 
     var artistasFavoritados = [];
 
-    var musicasCadastradas = [];
+    var musicasCadastradas = [{nome: "Música Teste", albumNome:"Álbum Teste", ano: "1998", duracao:"200"}];
 
     var playlistsCadastradas = [{nome:"Playlist Teste", musicas:[{nome: "musica1", albumNome:"temalbumn", ano: "2", duracao:"2"}, 
                                                                 {nome: "musica2", albumNome:"hihi", ano: "3", duracao:"4"}]}];
@@ -56,9 +56,25 @@ app.config(function($stateProvider, $urlRouterProvider) {
           controller: 'controlePrincipal',
         };
 
+          var musicasState = {
+          name: 'musicas',
+          url: '/musicas',
+          templateUrl: 'paginas/musicas.html',
+          controller: 'controlePrincipal',
+        };
+
+          var albunsState = {
+          name: 'albuns',
+          url: '/albuns',
+          templateUrl: 'paginas/albuns.html',
+          controller: 'controlePrincipal',
+        };
+
         $stateProvider.state(homeState);
         $stateProvider.state(playlistsState);
         $stateProvider.state(favoritosState);
+        $stateProvider.state(musicasState);
+        $stateProvider.state(albunsState);
 
 
     });
